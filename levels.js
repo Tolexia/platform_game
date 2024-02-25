9cc02f8d0e8ba0491a8c6e6308316631123d93fe
Axel <axelcroizepro@gmail.com>
2024-02-25 22:13:46 +0100

    Changed instructions on mobile + fix going through platforms + fix jumpheight on mobiles

--- levels.js
+++ levels.js
@@ -1,15 +1,16 @@
+const isMobile = 'ontouchstart' in window
 const win_width = window.innerWidth
-let win_height = 'ontouchstart' in window  ? window.innerHeight * 0.9 : window.innerHeight
+let win_height = isMobile  ? window.innerHeight * 0.9 : window.innerHeight
 
 function level0()
 {
-    instructions.innerHTML = ("Use Left and Right arrows to move")
+    instructions.innerHTML = isMobile ? "Press Left or Right joystick to move" : ("Use Left and Right arrows to move")
     platforms = []
     exit = new exit_door(0.85*win_width, win_height)
 }
 function level1()
 {
-    instructions.innerHTML = ("Use Up arrow to jump")
+    instructions.innerHTML = isMobile ? "Touch the game screen to jump" : ("Use Up arrow to jump")
     platforms = [
         new platform(0.2*win_width,0.9*win_height, 0.1*win_width, ball.radius * 5),
         new platform(0.4*win_width,0.8*win_height, 0.1*win_width, ball.radius * 5),
@@ -26,12 +27,11 @@ function level2()
         new platform(0.6*win_width,0.7*win_height, 0.1*win_width, ball.radius * 5),
         new platform(0.8*win_width,0.6*win_height, 0.1*win_width, ball.radius * 5),
 
-        new platform(0.7*win_width,0.5*win_height, 0.1*win_width, ball.radius * 5),
-        new platform(0.5*win_width,0.4*win_height, 0.1*win_width, ball.radius * 5),
-        new platform(0.3*win_width,0.3*win_height, 0.1*win_width, ball.radius * 5),
-        new platform(0.1*win_width,0.2*win_height, 0.1*win_width, ball.radius * 5),
+        new platform(0.6*win_width,0.5*win_height, 0.1*win_width, ball.radius * 5),
+        new platform(0.4*win_width,0.4*win_height, 0.1*win_width, ball.radius * 5),
+        new platform(0.2*win_width,0.3*win_height, 0.1*win_width, ball.radius * 5),
     ]
-    exit = new exit_door(0.15*win_width,0.2*win_height)
+    exit = new exit_door(0.25*win_width,0.3*win_height)
 }
 function level3()
 {
